b79f1080098150c3765ed5e0b02fbed19a2adf8a
mbravop <mbravop03@gmail.com>
2023-11-29 09:24:08 -0500

    Componentes compartidos

--- NetflixScope/src/app/app.component.ts
+++ NetflixScope/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BackToTopComponent } from './shared/back-to-top/back-to-top.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent, BackToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
